4f52e69747ed30bee4a2770947799f7415047f7f
Space <antariksh.mankar43@gmail.com>
2025-08-02 12:41:39 +0530

    Leetcode

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+class Palindrome {
+    public boolen isPalindrome(int x){
+        // x = 121 -> 121 -> Palindrome
+        if(n<){
+            return false;
+        }
+        int n = x ;
+        int revNum = 0;
+        while(n>0){
+            ind d = n%10;
+            revNumb = revNumx10 + d;
+            n = n/10;
+
+        }
+        if(revNum == x ){
+            return true;
+        }ele{
+            return false;
+        }
+    }
+}
