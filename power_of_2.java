0ba4d4246ddf44c85eeda6d26b7af37c1799aa73
space0032 <antariksh.mankar43@gmail.com>
2025-08-03 06:30:19 +0000

    power of two

---
+++ power_of_2.java
@@ -0,0 +1,19 @@
+class power_of_2 {
+    public boolean isPowerOfTwo(int n){
+        if(n<0){
+            return false;
+        }else if (n==1){
+            return true;
+
+        }else{
+            while(n%2==0){
+                n = n/2;
+            }
+            if (n==1){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+}
